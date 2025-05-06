186d65818714978299e5ad55e3d346b72ae26405
Neo Sun <huachuang20@gmail.com>
2025-05-06 20:54:04 +1200

    add models to graphql

--- src/schema/root.rs
+++ src/schema/root.rs
@@ -1,19 +1,45 @@
 //! The query root for jamscan
 
-use async_graphql::{Context, Object};
+use async_graphql::{Context, Object, Result};
 use sqlx::PgPool;
+
+use crate::models::{Epoch, Header, Ticket};
 
 /// Query root for jamscan
 pub struct QueryRoot;
 
 #[Object]
 impl QueryRoot {
-    async fn headers(&self, ctx: &Context<'_>) -> Vec<String> {
-        let pool = ctx.data::<PgPool>().unwrap();
-        let headers: Vec<String> = sqlx::query_scalar!("SELECT hash FROM headers")
-            .fetch_all(pool)
-            .await
-            .unwrap();
-        headers
+    async fn headers(
+        &self,
+        ctx: &Context<'_>,
+        #[graphql(default = 0)] offset: i64,
+        #[graphql(default = 10)] limit: i64,
+    ) -> Result<Vec<Header>> {
+        let pool = ctx.data::<PgPool>()?;
+        let data = Header::list(pool, offset, limit).await?;
+        Ok(data)
+    }
+
+    async fn tickets(
+        &self,
+        ctx: &Context<'_>,
+        #[graphql(default = 0)] offset: i64,
+        #[graphql(default = 10)] limit: i64,
+    ) -> Result<Vec<Ticket>> {
+        let pool = ctx.data::<PgPool>()?;
+        let data = Ticket::list(pool, offset, limit).await?;
+        Ok(data)
+    }
+
+    async fn epoches(
+        &self,
+        ctx: &Context<'_>,
+        #[graphql(default = 0)] offset: i64,
+        #[graphql(default = 10)] limit: i64,
+    ) -> Result<Vec<Epoch>> {
+        let pool = ctx.data::<PgPool>()?;
+        let data = Epoch::list(pool, offset, limit).await?;
+        Ok(data)
     }
 }
