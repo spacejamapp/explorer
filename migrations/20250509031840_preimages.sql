a2c570d8a8ecd70da74fcf9d1ea4f78796348d59
Neo Sun <huachuang20@gmail.com>
2025-05-09 19:02:51 +1200

    feat(block): add preimage hash to db

--- migrations/20250509031840_preimages.sql
+++ migrations/20250509031840_preimages.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS preimages (
   id SERIAL PRIMARY KEY,
   block INT NOT NULL REFERENCES blocks(slot),
   requester INT NOT NULL,
+  hash VARCHAR NOT NULL,
   blob BYTEA NOT NULL
 );
 CREATE INDEX idx_preimages_block ON preimages (block);
